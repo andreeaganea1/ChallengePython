58d1a7c9e79616d301cb7b366570c69695a203f7
andreeaganea1 <andreea.ganea.ceb@immune.institute>
2021-11-24 12:27:48 +0100

    Solucion al Challenge 1

---
+++ 1.Strings/challenge1_aganea.py
@@ -0,0 +1,16 @@
+#Strings
+#Ejercicio 1:
+
+print("Welcome to letter counter app!")
+name = input("Whats your name? ").title()
+print(f"Hello {name}! \nI will count the number of times that a specific letter occurs in your message.")
+
+message = input("Please enter a message: ").lower()
+letter = input("Wich letter would you like to count the occurences of? ").lower()
+
+number = 0
+
+for x in range (len(message)):
+    if message[x] == letter:
+        number += 1
+print(f"{name} your phrase has {number}{letter}'s in it.")
